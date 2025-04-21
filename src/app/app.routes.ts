54b5cd8df330086335c3bb21a509f37c5b3115ef
Tamires <tamires.pereira@jacto.com>
2025-04-21 09:58:13 -0300

    add navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'categories',
+    loadChildren: () =>
+      import('./pages/categories/categories.module')
+        .then(m => m.CategoriesModule)
+  }
+];
